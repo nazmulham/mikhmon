a548c8ed02a9e71755049fe362ff7c3795f82296
Nazmul Islam <41779390+nazmulham@users.noreply.github.com>
2023-08-06 11:24:47 +0600

    Update version.php

--- include/version.php
+++ include/version.php
@@ -2,6 +2,6 @@
 if (!isset($_SESSION["mikhmon"])) {
     header("Location:../admin.php?id=login");
   } else {
-        $_SESSION["v"] = "3.18 09-01-2019";
+        $_SESSION["v"] = "3.20 06-30-2021";
     
     }
